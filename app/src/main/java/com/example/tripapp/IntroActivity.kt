00e97b77b8a241c674c88b7f16cf62ba84e0c9fe
ml1010 <yke3@naver.com>
2022-02-21 21:08:07 +0900

    초기화면 로딩

---
+++ app/src/main/java/com/example/tripapp/IntroActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tripapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.intro_activity)
+
+        var handler = Handler()
+        handler.postDelayed( { var intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+                             }, 3000) }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
--- app/src/main/java/com/example/tripapp/MainActivity.kt
+++ app/src/main/java/com/example/tripapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tripapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SystemClock.sleep(3000) //3초 후 화면 전환
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+        finish()
     }
 }
